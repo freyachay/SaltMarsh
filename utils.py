da21973ad61d7b08cdfa8a4751ea453a87f38d11
Nolan Walsh <njabswalsh@gmail.com>
2017-06-05 04:00:29 +0000

    Editing utils

--- utils.py
+++ utils.py
@@ -15,7 +15,27 @@ def unpatch(y, num_patches_wide, num_patches_high):
     y = y.reshape(W, num_patches_wide, num_patches_high, H)
     y = y.transpose(1, 0, 2, 3)
     return y.reshape(num_patches_wide * W, num_patches_high * H)
+
+# Credit to nshaud 
+# (https://github.com/nshaud/DeepNetsForEO/blob/master/notebooks/Image%20extraction.ipynb) for general outline of making patches.
+def sliding_window(image, patch_size, step_size):
+    """Extract chips using a sliding window
     
+    Args:
+        image (numpy array): The image to be processed, must be 3 dimensional.
+        stride (int): The sliding window stride.
+        patch_size(int): The patch size.
+
+    Returns:
+        list: list of patches with patch_size dimensions
+    """
+    patches = []
+    for i in range(0, image.shape[0], step_size):
+        for j in range(0, image.shape[1], step_size):
+            new_patch = image[i:i+patch_size, j:j+patch_size, :]
+            if (new_patch.shape[0], new_patch.shape[1]) == (patch_size, patch_size):
+                patches.append(new_patch)
+    return patches
 
 def precision(y_true, y_pred):
     """Precision metric.
@@ -82,27 +102,33 @@ def report_history(hist):
     plt.legend(['train', 'test'], loc='upper left')
     plt.show()
     
-def display_tile(image, labels):
+def display_tile(image, labels, fig_size=(15,15), save_plot_path=None, dpi=None):
     """
     image: raw, unormalized
     labels: 0 or 1
     """
     # image only
-    plt.subplot(1, 3, 1)
+    fig = plt.figure(figsize = fig_size)
+    sub1 = fig.add_subplot(1, 3, 1)
     image = image.astype('float64')
     image *= 255.0/np.max(image)  # normalize
     image = image.astype('uint8')
-    plt.imshow(image)
+    sub1.imshow(image)
     
     # labels only
-    plt.subplot(1, 3, 2)
-    plt.imshow(labels, cmap="gray_r")
+    sub2 = fig.add_subplot(1, 3, 2)
+    sub2.imshow(labels, cmap="gray_r")
     
     # both
-    plt.subplot(1, 3, 3)
+    sub3 = fig.add_subplot(1, 3, 3)
 #     plt.imshow(image)
 #     plt.imshow(labels, cmap="gray_r", alpha=0.3)
     image[labels.astype("bool"), 2] = 200
-    plt.imshow(image)
+    sub3.imshow(image)
     
+    if save_plot_path != None:
+        if dpi != None:
+            plt.savefig(save_plot_path, dpi=dpi)
+        else:
+            plt.savefig(save_plot_path)
     plt.show()
